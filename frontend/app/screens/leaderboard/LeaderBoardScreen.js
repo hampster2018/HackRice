b9f69ad4e51eb2a37230ec089f04b9c5c6d5b20e
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2023-09-23 17:24:42 -0500

    functional leaderboard with podium

--- frontend/app/screens/leaderboard/LeaderBoardScreen.js
+++ frontend/app/screens/leaderboard/LeaderBoardScreen.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { SceneMap, TabView } from "react-native-tab-view";
+import { SceneMap, TabBar, TabView } from "react-native-tab-view";
 
 import Leaderboard from "./Leaderboard";
+import theme from "../../theme/Theme";
 
-const SLBRoute = () => <Leaderboard region="slb" />;
+const FirstTab = () => <Leaderboard groupId="1" />;
 
-const StateRoute = () => <Leaderboard region="state" />;
+const SecondTab = () => <Leaderboard groupId="2" />;
 
 const renderScene = SceneMap({
-  slb: SLBRoute,
-  state: StateRoute,
+  bfs: FirstTab,
+  slb: SecondTab,
 });
+
+const renderTabBar = (props) => (
+  <TabBar
+    {...props}
+    indicatorStyle={{ backgroundColor: "white" }}
+    style={{ backgroundColor: theme.colors.green }}
+  />
+);
 
 export default function LeaderboardScreen() {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
+    { key: "bfs", title: "Best Friends" },
     { key: "slb", title: "SLB" },
-    { key: "state", title: "Regional" },
   ]);
 
   return (
@@ -24,6 +33,9 @@ export default function LeaderboardScreen() {
       navigationState={{ index, routes }}
       renderScene={renderScene}
       onIndexChange={setIndex}
+      tabStyle={{ borderRadius: 10, backgroundColor: "#fff" }}
+      renderTabBar={renderTabBar}
+      // renderTabBar={CustomTabBar}
     />
   );
 }
--- frontend/app/screens/leaderboard/Leaderboard.js
+++ frontend/app/screens/leaderboard/Leaderboard.js
@@ -1,35 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
-import { useSelector } from "react-redux";
 
 import LeaderboardEntry from "./LeaderboardEntry";
-import SLBData from "./slb.leaderboard.json";
-import TXData from "./tx.leaderboard.json";
+import Podium from "./Podium";
 
-export default function Leaderboard({ region }) {
-  const entries = useSelector((state) => {
-    switch (region) {
-      case "slb":
-        return SLBData;
-      case "zipcode":
-        return state.zipcodeEntries;
-      case "city":
-        return state.cityEntries;
-      case "state":
-        return TXData;
-      default:
-        return [];
-    }
-  });
+export default function Leaderboard({ groupId }) {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    const fetchLeaderboard = async () => {
+      try {
+        const response = await fetch(
+          `https://highly-boss-dodo.ngrok-free.app/get_group_leaderboard/${groupId}/1`
+        );
+        const data = await response.json();
+        setEntries(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchLeaderboard();
+  }, [groupId]);
+
+  if (!entries) return null;
 
   return (
     <View style={styles.container}>
-      {entries.slice(0, 5).map((entry, index) => (
+      <Podium leaderboardData={entries.slice(0, 3)} />
+      <View
+        style={{
+          marginTop: 20,
+          marginBottom: 10,
+          borderBottomWidth: 1,
+          borderBottomColor: "black",
+          width: "90%",
+        }}
+      />
+      {entries.slice(3, entries.length).map((entry, index) => (
         <LeaderboardEntry
           key={index}
           name={entry.name}
           score={entry.score}
-          rank={index + 1}
+          rank={index + 4}
         />
       ))}
     </View>
